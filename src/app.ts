6c30fb7f686ff286a3faf6872539feab4901902f
reazul7 <islamreazul7777@gmail.com>
2024-12-21 11:53:54 +0600

    Create student, get all and single student

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/Student/student.route'
 
 const app: Application = express()
 
 // parser
 app.use(express.json())
 app.use(cors())
+
+// application route
+app.use('/api/v1/students', StudentRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
