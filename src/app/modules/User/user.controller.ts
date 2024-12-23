260fbfbc328737109183b191eb3b30371b294891
reazul7 <islamreazul7777@gmail.com>
2024-12-23 15:24:13 +0600

    use catchAsync function to avoid try-catch repetition

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,30 +1,19 @@
-import { NextFunction, Request, Response } from 'express'
 import { UserService } from './user.service'
 import { StatusCodes } from 'http-status-codes'
 import sendResponse from '../../utils/sendResponse'
+import catchAsync from '../../utils/catchAsync'
 
-const createStudent = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    try {
-        const { password, student: studentData } = req.body
-        // const zodParsedData = studentValidationSchema.parse(studentData)
-        const result = await UserService.createStudentIntoDB(
-            password,
-            studentData,
-        )
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student: studentData } = req.body
+    // const zodParsedData = studentValidationSchema.parse(studentData)
+    const result = await UserService.createStudentIntoDB(password, studentData)
 
-        sendResponse(res, {
-            statusCode: StatusCodes.OK,
-            success: true,
-            message: 'Student created successfully',
-            data: result,
-        })
-    } catch (error) {
-        next(error)
-    }
-}
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Student created successfully',
+        data: result,
+    })
+})
 
 export const UserControllers = { createStudent }
