5bd6c9390575a8da3c17271aa752032746464c25
reazul7 <islamreazul7777@gmail.com>
2025-01-06 19:19:51 +0600

    handle unhandledRejection  Error and uncaughtException Error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -8,9 +8,10 @@ import handleZodError from '../errors/handleZodError'
 import handleValidationError from '../errors/handleValidationError'
 import handleCastError from '../errors/handleCastError'
 import handleDuplicateError from '../errors/handleDuplicateError'
+import AppError from '../errors/AppError'
 const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-    let statusCode = error.statusCode || 500
-    let message = error.message || 'Something went wrong!'
+    let statusCode = 500
+    let message = 'Something went wrong!'
 
     let errorSources: TErrorSource = [
         {
@@ -39,6 +40,19 @@ const globalErrorHandler = (error: any, req: Request, res: Response, next: NextF
         statusCode = simplifiedError?.statusCode
         message = simplifiedError?.message
         errorSources = simplifiedError?.errorSources
+    } else if (error instanceof AppError) {
+        statusCode = error?.statusCode
+        message = error?.message
+        errorSources = [{
+            path: '',
+            message: error?.message,
+        }]
+    } else if (error instanceof Error) {
+        message = error?.message
+        errorSources = [{
+            path: '',
+            message: error?.message,
+        }]
     }
 
     return res.status(statusCode).json({
