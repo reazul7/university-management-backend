a1c65374e17b65b780920ca49f1872d54ac94bcd
reazul7 <islamreazul7777@gmail.com>
2025-03-03 11:06:36 +0600

    Calculate total marks, grade and points

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -134,11 +134,15 @@ const updateEnrolledCourseMarksIntoDB = async (facultyId: string, payload: Parti
     }
 
     const modifiedData: Record<string, unknown> = { ...courseMarks }
-    if(courseMarks?.finalTerm){
-        const {classTest1, classTest2, midTerm, finalTerm} = courseMarks
-        const totalMarks = Math.ceil(classTest1*0.10)+Math.ceil(classTest2*0.10)+Math.ceil(midTerm*0.30)+Math.ceil(finalTerm*0.50)
+    if (courseMarks?.finalTerm) {
+        const { classTest1, classTest2, midTerm, finalTerm } = courseMarks
+        const totalMarks =
+            Math.ceil(classTest1 * 0.1) +
+            Math.ceil(classTest2 * 0.1) +
+            Math.ceil(midTerm * 0.3) +
+            Math.ceil(finalTerm * 0.5)
         const result = calculateGradeAndPoints(totalMarks)
-        
+
         modifiedData.grade = result.grade
         modifiedData.gradePoints = result.gradePoints
         modifiedData.isCompleted = true
--- src/app/modules/EnrolledCourse/enrolledCourse.utils.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.utils.ts
@@ -1,27 +1,26 @@
 export const calculateGradeAndPoints = (totalMarks: number) => {
     const grades = [
-        { min: 0, max: 39, grade: 'F', gradePoints: 0.00 },
-        { min: 40, max: 44, grade: 'D', gradePoints: 2.00 },
+        { min: 0, max: 39, grade: 'F', gradePoints: 0.0 },
+        { min: 40, max: 44, grade: 'D', gradePoints: 2.0 },
         { min: 45, max: 49, grade: 'C', gradePoints: 2.25 },
-        { min: 50, max: 54, grade: 'C+', gradePoints: 2.50 },
+        { min: 50, max: 54, grade: 'C+', gradePoints: 2.5 },
         { min: 55, max: 59, grade: 'B-', gradePoints: 2.75 },
-        { min: 60, max: 64, grade: 'B', gradePoints: 3.00 },
+        { min: 60, max: 64, grade: 'B', gradePoints: 3.0 },
         { min: 65, max: 69, grade: 'B+', gradePoints: 3.25 },
-        { min: 70, max: 74, grade: 'A-', gradePoints: 3.50 },
+        { min: 70, max: 74, grade: 'A-', gradePoints: 3.5 },
         { min: 75, max: 79, grade: 'A', gradePoints: 3.75 },
-        { min: 80, max: 100, grade: 'A+', gradePoints: 4.00 }
-    ];
+        { min: 80, max: 100, grade: 'A+', gradePoints: 4.0 },
+    ]
 
-    return grades.find(({ min, max }) => totalMarks >= min && totalMarks <= max) || { grade: 'N/A', gradePoints: 0 };
-};
-
+    return grades.find(({ min, max }) => totalMarks >= min && totalMarks <= max) || { grade: 'N/A', gradePoints: 0 }
+}
 
 // export const calculateGradeAndPoints = (totalMarks: number) => {
 //     let result = {
 //       grade: 'NA',
 //       gradePoints: 0,
 //     };
-  
+
 //     if (totalMarks >= 0 && totalMarks <= 39) {
 //       result = {
 //         grade: 'F',
@@ -78,6 +77,6 @@ export const calculateGradeAndPoints = (totalMarks: number) => {
 //         gradePoints: 0,
 //       };
 //     }
-  
+
 //     return result;
 //   };
